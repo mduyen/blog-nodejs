70585520c855f7f924b67265ed76699d6bb2b6b3
mduyen <94757750+mduyen@users.noreply.github.com>
2023-03-10 23:11:59 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'))
+//router
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+});
+
+// 127.0.0.1 => localhost
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
